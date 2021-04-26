7f19438cc9335ef837c3a4fc411ebc97ab7a3834
= <=>
2021-04-26 16:57:45 +0800

    Add the controller

---
+++ controller.js
@@ -0,0 +1,65 @@
+import Cache from "./cache.js"
+import query from "./query.js"
+
+class Controller extends Cache {
+
+	constructor(url) {
+		super(url)
+
+		// 绑定this指向
+		this.all = this.all.bind(this)
+		this.title = this.title.bind(this)
+		this.icons = this.icons.bind(this)
+
+		// 判断是否存在缓存, 不存在返回一个异步函数获取数据
+		if (!super.exist) return (async () => {
+			// 获取标题和图标
+			this.data = await query(url.href)
+			// 返回this
+			return this
+		})()
+
+	}
+
+
+	all() {
+		// title
+		const { title } = this.title()
+
+		// icons
+		const { icons } = this.icons()
+
+		// return
+		return {
+			title,
+			icons
+		}
+	}
+
+	title() {
+		// 直接返回无需处理
+		return { title: this.data.title }
+
+	}
+
+	icons() {
+		// 赋值
+		let icons = this.data.icons
+
+		// 判断是否是默认路径
+		if (icons == '/favicon.ico') {
+			icons = this.url.origin + icons
+		}
+
+		// 判断是否带有http文本
+		if (icons.indexOf('http') == -1) {
+			// 在字符串前面添加
+			icons = this.url.protocol + icons
+		}
+
+		// 返回
+		return { icons }
+	}
+}
+
+export default Controller
