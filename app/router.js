e1f74e5e8a7230c9b3e20d0b000b8813832c2ceb
ZoftTy <poledye@icloud.com>
2021-08-25 11:45:28 +0800

    update / router

--- app/router.js
+++ app/router.js
@@ -1,14 +1,13 @@
 import Router from 'koa-router'
 import IndexController from './controller/index.js'
 import ManageController from './controller/manage.js'
-import package from '../package.json'
 
 // 实例化
 const router = new Router()
 const index = new IndexController()
 const manage = new ManageController()
 
-router.get('/', ctx => ctx.redirect(package.repository))
+router.get('/', ctx => ctx.redirect('https://github.com/ZoftTy/WebsitesInfo-API.git'))
 
 // 获取网站所有信息
 router.post('/api/info', ctx => index.index(ctx))
